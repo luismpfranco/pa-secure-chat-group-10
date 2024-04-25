cd55c80d698e446828289db08af1d0ab9dff6455
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-04-25 12:23:44 +0100

    feat: modify sendMessage and receiveMessage for multiple receivers and decryption

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,41 +1,122 @@
 package org.example;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.security.KeyPair;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 
-public class Client {
+public class Client
+{
     private String name;
     private String username;
-    private KeyPair keyPair;
+    private PrivateKey privateRSAKey;
+    private final PublicKey publicRSAKey;
     public Certificate certificate;
 
-    public Client(String name, String username,KeyPair keyPair) {
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Socket client;
+    private Server server;
+
+    private ClientWindow window;
+
+    public Client(String name, String username, Server server) throws Exception {
         this.name = name;
         this.username = username;
-        this.keyPair = keyPair;
+        KeyPair keyPair = Encryption.generateKeyPair ( );
+        this.privateRSAKey = keyPair.getPrivate();
+        this.publicRSAKey = keyPair.getPublic();
+        this.server = server;
+        window = new ClientWindow(this, server);
     }
 
-    public void receiveMessage(String message, Client sender, Client receiver){
-        System.out.println("Message from " + sender.getName() + ": " + message + " to " + receiver.getName());
+    public void sendMessage(String message, Client... receivers) throws Exception {
+        byte[] encryptedMessage;
+        byte[] digest = Encryption.createDigest(message);
+        Message messageObj;
+
+        StringBuilder receiverNames = new StringBuilder();
+        for (Client r : receivers) {
+            if(r == this)
+            {
+                continue;
+            }
+            receiverNames.append("@").append(r.getName()).append(", ");
+            encryptedMessage = Encryption.encryptRSA(message, r.getPublicRSAKey());
+            messageObj = new Message(encryptedMessage, digest);
+            server.forwardMessage(messageObj, this, r);
+
+        }
+
+        if(!receiverNames.isEmpty()){
+            receiverNames.setLength(receiverNames.length() - 2);
+        }
+        window.addMessage(receiverNames + " " + message);
     }
 
-    public String getName() {
+    public void receiveMessage(Message message, Client sender, Client receiver) throws Exception {
+        String decryptedMessage = Encryption.decryptRSA(message.getMessage(), this.getPrivateRSAKey());
+        if (!Encryption.verifyDigest(decryptedMessage, message.getDigest())) {
+            throw new Exception("Message integrity check failed");
+        }
+        if(sender != this)
+        {
+            window.addMessage(sender.getUsername() + ": " + decryptedMessage);
+        }
+    }
+
+    public String getName()
+    {
         return name;
     }
 
-    public String getUsername() {
+    public String getUsername()
+    {
         return username;
     }
-    public Certificate getCertificate() {
+
+    public Certificate getCertificate()
+    {
         return certificate;
     }
-    public void setCertificate(Certificate certificate) {
+
+    public void setCertificate(Certificate certificate)
+    {
         this.certificate = certificate;
     }
 
-    public void receiveCertificate(Certificate certificate) {
+    public void receiveCertificate(Certificate certificate)
+    {
         this.certificate = certificate;
+    }
+
+    public PublicKey getPublicRSAKey()
+    {
+        return publicRSAKey;
+    }
+
+    public PrivateKey getPrivateRSAKey()
+    {
+        return privateRSAKey;
+    }
+
+    public Server getServer()
+    {
+        return server;
+    }
+
+    public ClientWindow getWindow()
+    {
+        return window;
+    }
+
+    @Override
+    public String toString() {
+        return getUsername();
     }
 }
