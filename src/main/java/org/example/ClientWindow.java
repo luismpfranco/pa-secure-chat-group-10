96d264eb50c1c8bbad73bc738f5d44b6434319b3
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-04-25 20:50:13 +0100

    feat(ClientWindow): improve the class to remove the warnings

--- src/main/java/org/example/ClientWindow.java
+++ src/main/java/org/example/ClientWindow.java
@@ -8,11 +8,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ClientWindow extends JFrame {
-    private Client client;
-    private JTextArea messagesArea;
-    private JTextField inputField;
-    private JComboBox<Client> clientComboBox;
-    private JButton broadcastButton;
+    private final Client client;
+    private final JTextArea messagesArea;
+    private final JTextField inputField;
+    private final JComboBox<Client> clientComboBox;
     private static int windowCount = 0;
     private static final int WINDOW_DISTANCE = 360;
 
@@ -22,6 +21,7 @@ public class ClientWindow extends JFrame {
         setTitle(client.getUsername());
         setSize(350, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         setLocation(windowCount * WINDOW_DISTANCE, WINDOW_DISTANCE);
         windowCount++;
 
@@ -54,18 +54,15 @@ public class ClientWindow extends JFrame {
 
         add(topPanel, BorderLayout.NORTH);
 
-        broadcastButton = new JButton("Broadcast");
+        JButton broadcastButton = new JButton("Broadcast");
         add(broadcastButton, BorderLayout.EAST);
-        broadcastButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Client[] allClients = server.getClients().toArray(new Client[0]);
-                    client.sendMessage(inputField.getText(), allClients);
-                    inputField.setText("");
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+        broadcastButton.addActionListener(e -> {
+            try {
+                Client[] allClients = server.getClients().toArray(new Client[0]);
+                client.sendMessage(inputField.getText(), allClients);
+                inputField.setText("");
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         });
 
