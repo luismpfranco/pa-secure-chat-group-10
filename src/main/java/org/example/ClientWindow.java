607d9adffc9adf3d365c5f52a80675e6b12671ea
MariaCatarinaVF <catarinaf901@gmail.com>
2024-04-25 12:56:17 +0100

    feat(ClientWindow): Add "Para:" before select box, adjustment of the size of the window and horizontal distribution  of each window

--- src/main/java/org/example/ClientWindow.java
+++ src/main/java/org/example/ClientWindow.java
@@ -13,13 +13,17 @@ public class ClientWindow extends JFrame {
     private JTextField inputField;
     private JComboBox<Client> clientComboBox;
     private JButton broadcastButton;
+    private static int windowCount = 0;
+    private static final int WINDOW_DISTANCE = 360;
 
     public ClientWindow(Client client, Server server) {
         this.client = client;
 
         setTitle(client.getUsername());
-        setSize(500, 500);
+        setSize(350, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(windowCount * WINDOW_DISTANCE, WINDOW_DISTANCE);
+        windowCount++;
 
         messagesArea = new JTextArea();
         messagesArea.setEditable(false);
@@ -40,8 +44,15 @@ public class ClientWindow extends JFrame {
             }
         });
 
+
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel toLabel = new JLabel("Para:");
+        topPanel.add(toLabel);
+
         clientComboBox = new JComboBox<>(server.getClients().toArray(new Client[0]));
-        add(clientComboBox, BorderLayout.NORTH);
+        topPanel.add(clientComboBox);
+
+        add(topPanel, BorderLayout.NORTH);
 
         broadcastButton = new JButton("Broadcast");
         add(broadcastButton, BorderLayout.EAST);
