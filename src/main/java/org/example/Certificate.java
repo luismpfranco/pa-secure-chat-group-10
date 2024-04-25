b13d382c3c96d899e6b154382ab78b7d23106c26
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-04-25 20:48:22 +0100

    feat: add getters and setters

--- src/main/java/org/example/Certificate.java
+++ src/main/java/org/example/Certificate.java
@@ -3,15 +3,39 @@ package org.example;
 import java.security.PublicKey;
 
 class Certificate {
-    private PublicKey publicKey;
+    private final PublicKey publicKey;
     private boolean valid;
+    private final String username;
+    private String signature;
 
-    public Certificate(PublicKey publicKey) {
+    public Certificate(PublicKey publicKey, String username) {
         this.publicKey = publicKey;
-        this.valid = true; // Simulate certificate being initially valid
+        this.username = username;
+        this.signature = null;
+        this.valid = false;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
     }
 
     public boolean isValid() {
         return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
     }
 }
