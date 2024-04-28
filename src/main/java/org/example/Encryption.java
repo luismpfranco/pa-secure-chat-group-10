903ad1820103fe951e759984a4ce6f15ebc2c5c0
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-04-28 17:37:44 +0100

    refactor: delete unused methods

--- src/main/java/org/example/Encryption.java
+++ src/main/java/org/example/Encryption.java
@@ -1,8 +1,6 @@
 package org.example;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.ByteBuffer;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -57,38 +55,6 @@ public class Encryption {
         Cipher cipher = Cipher.getInstance ( "RSA" );
         cipher.init ( Cipher.DECRYPT_MODE , privateKey );
         return new String(cipher.doFinal(message));
-    }
-
-    /**
-     * @param message   the message to be decrypted
-     * @param secretKey the secret key used to decrypt the message
-     *
-     * @return the decrypted message as an array of bytes
-     *
-     * @throws Exception when the encryption fails
-     */
-    public static byte[] encryptAES ( byte[] message , byte[] secretKey ) throws Exception {
-        byte[] secretKeyPadded = ByteBuffer.allocate ( 16 ).put ( secretKey ).array ( );
-        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , "AES" );
-        Cipher cipher = Cipher.getInstance ( "AES/ECB/PKCS5Padding" );
-        cipher.init ( Cipher.ENCRYPT_MODE , secreteKeySpec );
-        return cipher.doFinal ( message );
-    }
-
-    /**
-     * @param message   the message to be encrypted
-     * @param secretKey the secret key used to encrypt the message
-     *
-     * @return the encrypted message as an array of bytes
-     *
-     * @throws Exception when the decryption fails
-     */
-    public static byte[] decryptAES ( byte[] message , byte[] secretKey ) throws Exception {
-        byte[] secretKeyPadded = ByteBuffer.allocate ( 16 ).put ( secretKey ).array ( );
-        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , "AES" );
-        Cipher cipher = Cipher.getInstance ( "AES/ECB/PKCS5Padding" );
-        cipher.init ( Cipher.DECRYPT_MODE , secreteKeySpec );
-        return cipher.doFinal ( message );
     }
 
     public static byte[] createDigest(String message) throws Exception {
